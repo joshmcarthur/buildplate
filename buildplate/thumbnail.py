4ab52e6aaaa702193219047d1e16616a3d92e29b
Josh McArthur <joshua.mcarthur@gmail.com>
2020-09-21 20:55:29 +1200

    Save thumbnail without opening a UI window

--- buildplate/thumbnail.py
+++ buildplate/thumbnail.py
@@ -11,4 +11,4 @@ def generate_thumbnail(infile, outfile, size=None):
 
     # Front of design and slightly up
     vpl.view(camera_position=[0, -1, 0.5])
-    vpl.save_fig(outfile, pixels=size or [1280, 1280])
+    vpl.save_fig(outfile, pixels=size or [1280, 1280], off_screen=True)
